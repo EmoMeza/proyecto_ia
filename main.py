0198ae7a0bfdcd5e3c8fb47d93f5ebc679ddf963
EmoMeza <emeza2019@inf.udec.cl>
2023-05-25 00:20:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,12 +11,9 @@ from poke_env import ServerConfiguration
 
 class MaxDamagePlayer(Player):
     def choose_move(self, battle):
-        # If the player can attack, it will
         if battle.available_moves:
-            # Finds the best move among available ones
             best_move = max(battle.available_moves, key=lambda move: move.base_power)
             return self.create_order(best_move)
-        # If no attack is available, a random switch will be made
         else:
             return self.choose_random_move(battle)
         
@@ -24,18 +21,10 @@ class MaxDamagePlayer(Player):
 
 async def main():
     start = time.time()
-
-    # We create two players.
     random_player = RandomPlayer(battle_format="gen8randombattle",save_replays=True)
     max_damage_player = MaxDamagePlayer(battle_format="gen8randombattle")
-
-    # Now, let's evaluate our player
     await max_damage_player.battle_against(random_player, n_battles=100)
-
-    print(
-        "Max damage player won %d / 100 battles [this took %f seconds]"
-        % (max_damage_player.n_won_battles, time.time() - start)
-    )
+    print("Max damage player won %d / 100 battles [this took %f seconds]"% (max_damage_player.n_won_battles, time.time() - start))
 
 
 if __name__ == "__main__":
